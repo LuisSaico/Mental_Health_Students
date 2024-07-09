c61d54ff7e90fc7f9dc2c84b6a03fe6ec2fa895b
Luis <luisezequiel.d6@gmail.com>
2024-07-09 17:40:31 -0600

    modifiying and importing MySQL

--- mental_health_students.py
+++ mental_health_students.py
@@ -7,8 +7,9 @@ conexión social y el estrés asociado a la incorporación a una nueva cultura. 
 de principiante consistirá en centrarte en un factor contribuyente específico: la duración de la estancia y cómo influye
 en las puntuaciones medias de diagnóstico de los estudiantes internacionales."""
 
+import mysql.connector
+
 # Lists
-mental_healt_problem = []
 invalid_dates = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
 # CREATING FATHER CLASS
@@ -25,33 +26,60 @@ class Person:
 # CREATING SON CLASS
 class Student(Person):
 
-    def __init__(self, name, age, gender, student_id):
+    def __init__(self, name, age, gender, student_id, mental_problem):
         super().__init__(name, age, gender)
         self.student_id = student_id
+        self.mental_problem = mental_problem
 
     # Show Dates of the student
     def __str__(self):
         return (f"Name of the student: {self.name}\n"
                 f"Age of the student: {self.age}\n"
                 f"Gender of the student: {self.gender}\n"
-                f"Id of the student: {self.student_id}")
+                f"Id of the student: {self.student_id}\n"
+                f"Mental problem of the student: {self.mental_problem}")
 
     # Ask for the mental health problem
     @staticmethod
-    def ask():
+    def ask(limit_students):
 
-        problem = input("Enter the mental problem of the student: ")
+        students = []
 
-        # Evaluating invalid date
-        for number in invalid_dates:
-            if number in problem:
-                while True:
-                    problem = input("Enter the mental problem of the student: ")
-                    if number in problem:
-                        continue
-                    else:
-                        mental_healt_problem.append(problem)
-                        break
+        for _ in range(limit_students):
+
+            name = input("Enter the name of the student: ")
+            age = int(input("Enter the age of the student: "))
+            gender = input("Enter the gender of the estudent: ")
+            idd = input("Enter the id of the student: ")
+            problem = input("Enter the mental problem of the student: ")
+            student = Student(name, age, gender, idd, problem)
+            students.append(student)
+
+
+# MENU OF THE SYSTEM
+def menu(student):
+
+    print("||SAVING MIND||\n")
+
+    while True:
+
+        print("[1]-Register student\n"
+              "[2]-Show the students\n"
+              "[3]-Leave the system\n")
+
+        choice = int(input("Enter your choice: "))
+        print("=" * 30)
+
+        if choice == 1:
+            student.ask()
+
+        elif choice == 2:
+            print("showing")
+
+        elif choice == 3:
+            print("Leaving the system...")
+            print("Thanks.")
+
 
 
 
